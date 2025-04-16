5c566200417970abd65660bebd3276f8c1ba978b
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-16 09:31:36 -0700

    prompt.c

--- prompt.c
+++ prompt.c
@@ -6,24 +6,28 @@
  *
  * Return: number of characters
 */
-int main()
+int main(void)
 {
-    char *buffer;
-    size_t bufsize = 32;
-    size_t characters;
-
-    buffer = (char *)malloc(bufsize * sizeof(char));
-    if( buffer == NULL)
-    {
-        perror("Unable to allocate buffer");
-        exit(1);
-    }
-	while (1)
-	{
-		printf("$ ");
-    	characters = getline(&buffer,&bufsize,stdin);
-    	printf("%s\n",buffer);
-	}
-
-    return (characters);
+char *buffer;
+size_t bufsize = 32;
+ssize_t read;
+buffer = (char *)malloc(bufsize * sizeof(char));
+if (buffer == NULL)
+{
+perror("Unable to allocate buffer");
+exit(1);
+}
+while (1)
+{
+printf("$ ");
+read = getline(&buffer, &bufsize, stdin);
+if (read == -1)
+{
+printf("\n");
+break;
+}
+printf("%s", buffer);
+}
+free(buffer);
+return (0);
 }
