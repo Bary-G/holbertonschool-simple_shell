3ba003b80ff6d074a38eb6231e3da6a8a9cef655
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-18 19:44:13 -0700

    env-main.c

--- env-main.c
+++ env-main.c
@@ -7,15 +7,15 @@
  */
 int main(int ac, char **av, char **env)
 {
-	unsigned int i;
-
-	i = 0;
-	while (env[i] != NULL)
-	{
-		printf("%s\n", env[i]);
-		i++;
-	}
-	return (0);
-
+unsigned int i;
+(void)ac;
+(void)av;
+i = 0;
+while (env[i] != NULL)
+{
+printf("%s\n", env[i]);
+i++;
+}
+return (0);
 }
 
