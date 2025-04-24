e39ba2860f04c6035e80c7193b2d2955396e7245
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-24 09:19:45 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,18 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
 /**
  * struct path_node - singly linked list for PATH directories
  * @dir: a directory string
@@ -12,6 +24,21 @@ char *dir;
 struct path_node *next;
 } path_node;
 
+extern char **environ;
+/* Function Prototypes */
+char *readline(void);
+char **splitline(char *line);
+int execute(char **args);
+char *get_path(char *command);
+char *_strcat(char *dest, char *src);
+char **tokenize_env(char *path);
+int _strcmp(char *s1, char *s2);
+void sigint_handler(int sig);
+int _putchar(char c);
+char *_getenv(char *var);
+void _printenv(void);
+int _strlen(char *s);
+int _strncmp(char *str1, char *str2, int n);
 path_node *build_path_list(void);
 void print_path_list(path_node *head);
 void free_path_list(path_node *head);
