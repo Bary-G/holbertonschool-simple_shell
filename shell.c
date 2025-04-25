0d2d7d62fc3d9a0218d69c9b544f2ec31ba6decb
Bary-G <b9059289@gmail.com>
2025-04-25 10:30:36 +0200

    shell

--- shell.c
+++ shell.c
@@ -16,43 +16,46 @@ extern char **environ;
  */
 int main(void)
 {
-char *line = NULL;
-size_t len = 0;
-pid_t pid;
-int status;
-while (1)
-{
-printf(PROMPT);
-if (getline(&line, &len, stdin) == -1)
-{
-perror("getline");
-break;
-}
-line[strcspn(line, "\n")] = '\0';
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	int status;
 
-/* Fork and exec */
-pid = fork();
-if (pid == -1)
-{
-perror("fork");
-}
-else if (pid == 0)
-{
-char *argv[2];
-argv[0] = line;
-argv[1] = NULL;
-if (execve(line, argv, environ) == -1)
-{
-perror("execve");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-wait(&status);
-}
-}
-free(line);
-return (0);
+	while (1)
+	{
+		printf(PROMPT);
+		if (getline(&line, &len, stdin) == -1)
+		{
+			perror("getline");
+			break;
+		}
+
+		line[strcspn(line, "\n")] = '\0';
+
+		/* Fork and exec */
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork");
+		}
+		else if (pid == 0)
+		{
+			char *argv[2];
+			argv[0] = line;
+			argv[1] = NULL;
+			if (execve(line, argv, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	free(line);
+	return (0);
 }
 
