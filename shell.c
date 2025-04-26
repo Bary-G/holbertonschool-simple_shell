022783efb9fc10bb3bc2b29ffa58e6281ec6f7a3
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-25 18:42:28 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -8,59 +8,114 @@
 #define PROMPT "#cisfun$ "
 #define MAX_ARGS 64
 
+/**
+ * handle_input - Reads input from the user
+ * @line: Pointer to the input buffer
+ * @len: Pointer to the size of the buffer
+ * 
+ * Return: Returns the line of input, or NULL if an error occurs
+ */
+char *handle_input(char *line, size_t *len)
+{
+    if (getline(&line, len, stdin) == -1)
+    {
+        return (NULL);
+    }
+    line[strcspn(line, "\n")] = '\0';
+    return (line);
+}
+
+/**
+ * tokenize_input - Tokenizes the input string into arguments
+ * @line: The input line to be tokenized
+ * @args: The array to store the arguments
+ * 
+ * Return: The number of arguments
+ */
+int tokenize_input(char *line, char *args[])
+{
+    char *token;
+    int i = 0;
+
+    token = strtok(line, " \t");
+    while (token != NULL && i < MAX_ARGS - 1)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " \t");
+    }
+    args[i] = NULL;
+    return (i);
+}
+
+/**
+ * execute_command - Forks a process to execute the command
+ * @args: The array of arguments to execute
+ */
+void execute_command(char *args[])
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        return;
+    }
+
+    if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        wait(&status);
+    }
+}
+
+/**
+ * main - Main shell loop
+ * 
+ * Return: Always 0
+ */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	char *args[MAX_ARGS];
-	pid_t pid;
-	int status;
+    char *line = NULL;
+    size_t len = 0;
+    char *args[MAX_ARGS];
 
-	while (1)
-	{
-	if (isatty(STDIN_FILENO))
-	printf(PROMPT);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+        {
+            printf(PROMPT);
+        }
 
-	if (getline(&line, &len, stdin) == -1)
-	break;
+        line = handle_input(line, &len);
+        if (line == NULL)
+        {
+            break;f
+        }
 
-	line[strcspn(line, "\n")] = '\0';
+        if (line[0] == '\0')
+        {
+            continue;
+        }
 
-	if (line[0] == '\0')
-	continue;
+        if (strcmp(line, "exit") == 0)
+        {
+            break;
+        }
 
-	if (strcmp(line, "exit") == 0)
-	break;
-	char *token = strtok(line, " \t");
-	int i = 0;
-	while (token != NULL && i < MAX_ARGS - 1)
-{
-	args[i++] = token;
-	token = strtok(NULL, " \t");
-	}
-		args[i] = NULL;
-		pid = fork();
-		if (pid == -1)
-	{	
-		perror("fork");
-		continue;
-	}
-		if (pid == 0)
-{
-	if (execvp(args[0], args) == -1)
-{
-	perror("execvp");
-	exit(EXIT_FAILURE);
-	}	
-
-}
-	else
-{
-	wait(&status);
-}
-	}
+        tokenize_input(line, args); ts
+        execute_command(args);
+    }
 
     free(line);
-    return 0;
+    return (0);
 }
 
