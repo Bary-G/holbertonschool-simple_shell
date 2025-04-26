d4910fec1e9dadd64c7c760a54a0bc58153d9f79
Bary-G <b9059289@gmail.com>
2025-04-26 09:42:52 +0200

    shell

--- shell.c
+++ shell.c
@@ -2,42 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "shell.h"
 
 #define PROMPT "#cisfun$ "
+#define BUFFER_SIZE 1024
 
 /**
- * main - Entry point of the shell
- *
+ * main - Simple shell loop
  * Return: Always 0
  */
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	char *args[MAX_ARGS];
+	pid_t pid;
+	int status;
 
 	while (1)
 	{
-	if (isatty(STDIN_FILENO))
-	printf(PROMPT);
+		fflush(stdout);
+		if (isatty(STDIN_FILENO))
+		{
+			printf(PROMPT);
+		}
+		if (getline(&line, &len, stdin) == -1)
+		{
+			break;
+		}
+			line[_strcspn(line, "\n")] = '\0';
+			pid = fork();
 
-	if (getline(&line, &len, stdin) == -1)
-		break;
+			if (pid == -1)
+		{
+			perror("fork");
+		}
+			else if (pid == 0)
+		{
+			char *argv[2];
 
-	line[strcspn(line, "\n")] = '\0';
-
-	if (line[0] == '\0')
-		continue;
-
-	if (strcmp(line, "exit") == 0)
-		break;
-
-	parse_input(line, args);
-	execute_command(args);
+			argv[0] = line;
+			argv[1] = NULL;
+			if (execve(line, argv, environ) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		}
+			else
+		{
+			wait(&status);
+		}
 	}
-
 	free(line);
 	return (0);
 }
-
